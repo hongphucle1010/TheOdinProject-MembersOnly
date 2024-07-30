0847c552e88098bf7b07099fa10d5169bb8a16aa
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-07-30 14:11:45 +0700

    feat: Add express-validator for input validation in sign up controller

--- controllers/UserControllers/SignUpController.js
+++ controllers/UserControllers/SignUpController.js
@@ -1,14 +1,39 @@
 const { createUser } = require("../../models/UserModel/UserModel");
+const { body, validationResult } = require("express-validator");
 
-const signUpController = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await createUser(username, password);
-  if (user.error) {
-    res.render("signup", {
-      title: "Sign Up",
-      message: user.error,
-    });
-  } else res.redirect("/");
-};
+const signUpValidation = [
+  body("username")
+    .isLength({ min: 5 })
+    .withMessage("Username is too short")
+    .trim()
+    .notEmpty()
+    .withMessage("Username cannot be empty"),
+  body("password").isLength({ min: 2 }).withMessage("Password is too short"),
+];
+
+const signUpController = [
+  signUpValidation,
+  async (req, res) => {
+    const { username, password } = req.body;
+    const user = await createUser(username, password);
+    const validationErrors = validationResult(req)
+      .errors.map((error) => error.msg)
+      .join(". ");
+
+    // Add the validation errors to the user error message
+    if (validationErrors) {
+      user.error = user.error
+        ? `${user.error}. ${validationErrors}`
+        : validationErrors;
+    }
+
+    if (user.error) {
+      res.render("signup", {
+        title: "Sign Up",
+        message: user.error,
+      });
+    } else res.redirect("/");
+  },
+];
 
 module.exports = { signUpController };
--- routes/SignUpRouter/SignUpRouter.js
+++ routes/SignUpRouter/SignUpRouter.js
@@ -4,6 +4,7 @@ const {
   signUpController,
 } = require("../../controllers/UserControllers/SignUpController");
 
+
 router.get("/", (req, res) => {
   res.render("signup", {
     title: "Sign Up",
